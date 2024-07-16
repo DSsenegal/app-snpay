cf1c765ca4a11a98d010517754f8dd3ef624790c
Honorable Con <honorablecon@gmail.com>
2024-07-16 14:11:34 +0000

    ✅ completed #38

--- src/pages/api/webhooks/checkout-created.ts
+++ src/pages/api/webhooks/checkout-created.ts
@@ -1,29 +1,31 @@
 import { SaleorAsyncWebhook } from "@saleor/app-sdk/handlers/next";
 import {
-  OrderCreatedWebhookPayloadFragment,
-  UntypedOrderCreatedDocument,
+  CheckoutCreatedWebhookPayloadFragment,
+  UntypedCheckoutCreatedDocument,
 } from "../../../../generated/graphql";
 import { saleorApp } from "../../../saleor-app";
 import { createClient } from "../../../lib/create-graphq-client";
-import { createTransaction } from "@/services/create-transaction";
+import { createOrderFromCheckout } from "@/services/create-order-from-checkout";
 
 /**
  * Create abstract Webhook. It decorates handler and performs security checks under the hood.
  *
  * orderCreatedWebhook.getWebhookManifest() must be called in api/manifest too!
  */
-export const orderCreatedWebhook = new SaleorAsyncWebhook<OrderCreatedWebhookPayloadFragment>({
-  name: "Order Created in Saleor",
-  webhookPath: "/api/webhooks/order-created",
-  event: "ORDER_CREATED",
-  apl: saleorApp.apl,
-  query: UntypedOrderCreatedDocument,
-});
+export const checkoutCreatedWebhook = new SaleorAsyncWebhook<CheckoutCreatedWebhookPayloadFragment>(
+  {
+    name: "Checkout Created",
+    webhookPath: "/api/webhooks/checkout-created",
+    event: "CHECKOUT_CREATED",
+    apl: saleorApp.apl,
+    query: UntypedCheckoutCreatedDocument,
+  }
+);
 
 /**
  * Export decorated Next.js handler, which adds extra context
  */
-export default orderCreatedWebhook.createHandler(async (req, res, ctx) => {
+export default checkoutCreatedWebhook.createHandler(async (req, res, ctx) => {
   const {
     /**
      * Access payload from Saleor - defined above
@@ -46,36 +48,26 @@ export default orderCreatedWebhook.createHandler(async (req, res, ctx) => {
   /**
    * Perform logic based on Saleor Event payload
    */
-  console.log(`Order was created for customer: ${payload.order?.userEmail}`);
+  console.log(`Checkout was created ID: ${payload.checkout?.id}`);
 
   /**
    * Create GraphQL client to interact with Saleor API.
    */
   const client = createClient(authData.saleorApiUrl, async () => ({ token: authData.token }));
 
+  try {
+    const order = await createOrderFromCheckout(payload.checkout?.id as string, client);
+
+    console.log(order);
+  } catch (error) {
+    console.error("Error creating checkout", error);
+  }
   /**
    * Now you can fetch additional data using urql.
    * https://formidable.com/open-source/urql/docs/api/core/#clientquery
    */
 
   // const data = await client.query().toPromise()
-
-  try {
-    const transaction = await createTransaction(
-      {
-        orderId: payload.order?.id,
-        amount: payload.order?.total.gross.amount,
-        currency: payload.order?.total.gross.currency,
-        externalUrl: `${process.env.APP_IFRAME_BASE_URL}/checkout/${payload.order?.id}?saleorDomain=${authData.domain}`,
-        pspReference: "psp" + payload.order?.id,
-      },
-      client
-    );
-
-    console.log(transaction);
-  } catch (error) {
-    console.error("Error creating transaction", error);
-  }
 
   /**
    * Inform Saleor that webhook was delivered properly.
